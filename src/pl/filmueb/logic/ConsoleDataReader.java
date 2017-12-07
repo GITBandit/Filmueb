c880d11ddb08b71a40e905dd09d871a20cccebd4
Dominik Jakubowski <dominikjakubowski@gmail.com>
2017-12-08 00:45:11 +0100

    Tables and create methods moved to the ApplicationDatabase class.

--- src/pl/filmueb/logic/ConsoleDataReader.java
+++ src/pl/filmueb/logic/ConsoleDataReader.java
@@ -1,6 +1,7 @@
 package pl.filmueb.logic;
 
 import pl.filmueb.data.Actor;
+import pl.filmueb.data.ApplicationDatabase;
 import pl.filmueb.data.Movie;
 import pl.filmueb.data.Series;
 
@@ -10,98 +11,5 @@ public class ConsoleDataReader {
 
 
 
-    Actor[] actor = new Actor[10];
-    Movie[] movie = new Movie[10];
-    Series[] series = new Series[10];
-
-
-
-    Scanner scanner = new Scanner(System.in);
-
-    int movieCount;
-    Movie createMovie(){
-        movie[movieCount] = new Movie ();
-
-        System.out.println("Podaj tytuł filmu: ");
-        movie[movieCount].setName(scanner.nextLine());
-
-        System.out.println("Podaj reżysera filmu: ");
-        movie[movieCount].setDirector(scanner.nextLine());
-
-        System.out.println("Podaj rok w którym film był nagrany: ");
-        movie[movieCount].setYear(scanner.nextLine());
-
-        System.out.println("Podaj gatunek filmu: ");
-        movie[movieCount].setGenere(scanner.nextLine());
-
-        System.out.println("Podaj opis filmu: ");
-        movie[movieCount].setDescription(scanner.nextLine());
-
-        System.out.println("Podaj ocenę filmu: ");
-        movie[movieCount].setRating(scanner.nextDouble());
-        scanner.nextLine();
-        if(movie[movieCount].getRating()==0){
-            movie[movieCount] = null;
-            movieCount--;}
-
-        movieCount++;
-        return movie[movieCount];
-    }
-
-    int actorCount;
-    Actor createActor(){
-        actor[actorCount] = new Actor();
-
-        System.out.println("Podaj imię aktora: ");
-        actor[actorCount].setName(scanner.nextLine());
-
-        System.out.println("Podaj nazwisko aktora: ");
-        actor[actorCount].setSurname(scanner.nextLine());
-
-        System.out.println("Podaj narodowość aktora: ");
-        actor[actorCount].setNationality(scanner.nextLine());
-
-        actorCount++;
-
-        if (actorCount == 10) {
-            System.out.printf("Nie można utworzyć więcej aktorów!!!");
-        }
-        return actor[actorCount];
-
-    }
-
-    int seriesCount;
-    Series creteTvSeries(){
-        series[seriesCount] = new Series();
-
-        System.out.println("Podaj nazwę serialu: ");
-        series[seriesCount].setName(scanner.nextLine());
-
-        System.out.println("Podaj liczbę sezonów: ");
-        series[seriesCount].setNoOfSeries(scanner.nextInt());
-        scanner.nextLine();
-
-        System.out.println("Podaj liczbę odcinków: ");
-        series[seriesCount].setNoOfEpisodes(scanner.nextInt());
-        scanner.nextLine();
-
-        System.out.println("Podaj producenta: ");
-        series[seriesCount].setProducer(scanner.nextLine());
-
-        System.out.println("Podaj gatunek: ");
-        series[seriesCount].setGenre(scanner.nextLine());
-
-        System.out.println("Podaj opis: ");
-        series[seriesCount].setDescription(scanner.nextLine());
-
-        System.out.println("Podaj ocenę: ");
-        series[seriesCount].setRating(scanner.nextDouble());
-        scanner.nextLine();
-        if(series[seriesCount].getRating()==0){
-            series[seriesCount] = null;}
-
-        seriesCount++;
-        return series[seriesCount];
-    }
 
 }
--- src/pl/filmueb/logic/Filmueb.java
+++ src/pl/filmueb/logic/Filmueb.java
@@ -1,6 +1,7 @@
 package pl.filmueb.logic;
 
 import pl.filmueb.data.Actor;
+import pl.filmueb.data.ApplicationDatabase;
 import pl.filmueb.data.Movie;
 import pl.filmueb.data.Series;
 
@@ -9,13 +10,14 @@ import java.util.Scanner;
 public class Filmueb {
     public static void main(String[] args) {
 
-        System.out.println("Filmueb v0.2");
+        System.out.println("Filmueb v0.1");
 
         Scanner scanner = new Scanner(System.in);
 
         ConsoleDataReader reader = new ConsoleDataReader();
+        ApplicationDatabase database = new ApplicationDatabase();
 
-        int n = 0;
+        int n;
 
         for(int i = 1; i==1;) {
             System.out.println("\nWybierz opcję:\n" +
@@ -35,33 +37,33 @@ public class Filmueb {
 
             switch (n) {
                 case 1:
-                    reader.createMovie();
-                    if(reader.movie[reader.movieCount] == null ){
+                    database.createMovie();
+                    if(database.movie[database.movieCount] == null ){
                         System.out.println("Obiekt nie został utworzony.");
                     }
                     break;
                 case 2:
-                    reader.creteTvSeries();
-                    if (reader.series[reader.seriesCount] == null) {
+                    database.creteTvSeries();
+                    if (database.series[database.seriesCount] == null) {
                         System.out.println("Obiekt nie został utworzony.");
                     }
                     break;
                 case 3:
-                    reader.createActor();
+                    database.createActor();
                     //    Actor actorName = actorTest;         // referencja testowa - wpisujemy obiekt do przetestowania
                     break;
                 case 4:
-                    for (int z=0; z<reader.actorCount;z++ ){
+                    for (int z=0; z<database.actorCount;z++ ){
                         System.out.println("Aktor "+(z+1));
-                        System.out.printf("Imię %s, Nazwisko: %s, Narodowość: %s \n", reader.actor[z].getName(), reader.actor[z].getSurname(), reader.actor[z].getNationality());
+                        System.out.printf("Imię %s, Nazwisko: %s, Narodowość: %s \n", database.actor[z].getName(), database.actor[z].getSurname(), database.actor[z].getNationality());
                     }
-                    for (int x=0; x<reader.movieCount;x++){
+                    for (int x=0; x<database.movieCount;x++){
                         System.out.println("Film "+(x+1));
-                        System.out.printf("Tytuł: %s, Reżyser: %s, Rok: %s, Gatunek: %s \nOpis: %s\nOcena: %.2f \n", reader.movie[x].getName(), reader.movie[x].getDirector(), reader.movie[x].getYear(), reader.movie[x].getGenere(), reader.movie[x].getDescription(), reader.movie[x].getRating());
+                        System.out.printf("Tytuł: %s, Reżyser: %s, Rok: %s, Gatunek: %s \nOpis: %s\nOcena: %.2f \n", database.movie[x].getName(), database.movie[x].getDirector(), database.movie[x].getYear(), database.movie[x].getGenere(), database.movie[x].getDescription(), database.movie[x].getRating());
                     }
-                    for (int c=0; c<reader.seriesCount;c++){
+                    for (int c=0; c<database.seriesCount;c++){
                         System.out.println("Serial "+(c+1));
-                        System.out.printf("Tytuł: %s, Liczba sezonów: %d, Liczba odcinków: %d \nProducent: %s, Gatunek: %s \nOpis: %s \nOcena: %.2f \n", reader.series[c].getName(), reader.series[c].getNoOfSeries(), reader.series[c].getNoOfEpisodes(), reader.series[c].getProducer(), reader.series[c].getGenre(), reader.series[c].getDescription(), reader.series[c].getRating());
+                        System.out.printf("Tytuł: %s, Liczba sezonów: %d, Liczba odcinków: %d \nProducent: %s, Gatunek: %s \nOpis: %s \nOcena: %.2f \n", database.series[c].getName(), database.series[c].getNoOfSeries(), database.series[c].getNoOfEpisodes(), database.series[c].getProducer(), database.series[c].getGenre(), database.series[c].getDescription(), database.series[c].getRating());
                     }
                     break;
             }
